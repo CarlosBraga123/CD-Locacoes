f7eb4fd89e5fad5a471039771047e698cc7b95c4
CarlosBraga123 <carlosfellipecafe@hotmail.com>
2025-05-25 18:26:34 -0300

    Atualiza ícone e arquivos do app

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-});
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon-512.png'],
+      manifest: {
+        name: 'CD Locações',
+        short_name: 'CD Locações',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
